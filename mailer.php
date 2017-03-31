125b64d2c091570b7bdedc96a69c60f8ab13411d
Courtney Ring <CourtneyRing@MacBook-Pro.local>
2017-03-31 10:26:58 -0400

    mailer correction

--- mailer.php
+++ mailer.php
@@ -27,3 +27,35 @@
 ?>
 
 
+<?php
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+   // $from = 'hello@crawfordcountyhistoricalsociety.com';
+    $to = '"Courtney-Ring.com" <ce.ring@comcast.net>';
+    //$to = "ce.ring@comcast.net";
+    $subject = 'New Message from Courtney-Ring.com';
+    
+    $message = "From: $name\n E-Mail: $email\n Message:\n $message";
+    $headers = 'From: "Courtney-Ring.com" <hello@courtney-ring.com>' . "\r\n" . 'Reply-to:' . $email;
+
+        
+    if ($_POST['submit']) {
+        if (mail ($to, $subject, $message, $headers)) {
+            header("location:http://courtney-ring.com#contact");
+            //echo '<p class="success">Your message has been sent successfully!</p>';
+            //echo '<script>alert();</script>';
+
+       
+        } else {
+            echo "Error. Please go back and try again.";
+          
+        }
+    }
+
+?>
+
+
+
+
