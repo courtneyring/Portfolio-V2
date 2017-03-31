97e1d2d9d8ae5f4334055ab8bbd7329ba44c680b
Courtney Ring <ce.ring@comcast.net>
2017-03-31 16:12:48 -0400

    pull

--- contactMailer.php
+++ contactMailer.php
@@ -1,20 +1,21 @@
 <?php
+
     $name = $_POST['name'];
     $email = $_POST['email'];
     $message = $_POST['message'];
-    $url = $_POST['url'];
+
    // $from = 'hello@crawfordcountyhistoricalsociety.com';
-    $to = '"Crawford County Historical Society" <museumcchs97@gmail.com>';
+    $to = '"Courtney-Ring.com" <ce.ring@comcast.net>';
     //$to = "ce.ring@comcast.net";
-    $subject = 'New Message from CrawfordCountyHistoricalSociety.com';
+    $subject = 'New Message from Courtney-Ring.com';
     
     $message = "From: $name\n E-Mail: $email\n Message:\n $message";
-    $headers = 'From: "Crawford County Historical Society" <hello@crawfordcountyhistoricalsociety.com>' . "\r\n" . 'Reply-to:' . $email;
+    $headers = 'From: "Courtney-Ring.com" <hello@courtney-ring.com>' . "\r\n" . 'Reply-to:' . $email;
 
         
     if ($_POST['submit']) {
         if (mail ($to, $subject, $message, $headers)) {
-            header("Location: $url");
+            header("Location:http://www.courtney-ring.com#contact");
             //echo '<p class="success">Your message has been sent successfully!</p>';
             //echo '<script>alert();</script>';
 
@@ -26,5 +27,7 @@
     }
 
 ?>
+
+
 
 
