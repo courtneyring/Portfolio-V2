757c0bf8a1efa609ad38864b9a9c98fdcb800452
Courtney Ring <CourtneyRing@MacBook-Pro.local>
2017-06-15 22:53:50 -0400

    recaptcha

--- mailer.php
+++ mailer.php
@@ -1,4 +1,23 @@
 <?php
+
+    require_once "recaptchalib.php";
+    
+        // your secret key
+    $secret = "6LcePAATAAAAABjXaTsy7gwcbnbaF5XgJKwjSNwT";
+
+    // empty response
+    $response = null;
+
+    // check secret key
+    $reCaptcha = new ReCaptcha($secret);
+
+    if ($_POST["g-recaptcha-response"]) {
+    $response = $reCaptcha->verifyResponse(
+        $_SERVER["REMOTE_ADDR"],
+        $_POST["g-recaptcha-response"]
+    );
+}
+
     ini_set('SMTP','localhost'); 
     ini_set('sendmail_from', 'hello@courtney-ring.com'); 
 
