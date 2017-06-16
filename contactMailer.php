5f1ab7ce5c31f5bac77e1f8cf489b16d49332f4a
Courtney Ring <CourtneyRing@MacBook-Pro.local>
2017-06-15 22:56:49 -0400

    recaptcha

--- contactMailer.php
+++ contactMailer.php
@@ -1,5 +1,21 @@
 <?php
+ require_once "recaptchalib.php";
 
+            // your secret key
+        $secret = "6LcePAATAAAAABjXaTsy7gwcbnbaF5XgJKwjSNwT";
+
+        // empty response
+        $response = null;
+
+        // check secret key
+        $reCaptcha = new ReCaptcha($secret);
+
+        if ($_POST["g-recaptcha-response"]) {
+        $response = $reCaptcha->verifyResponse(
+            $_SERVER["REMOTE_ADDR"],
+            $_POST["g-recaptcha-response"]
+        );
+    }
     $name = $_POST['name'];
     $email = $_POST['email'];
     $message = $_POST['message'];
